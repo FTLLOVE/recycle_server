b93cfcd01ebdc91768fd5e608f433b422bbd7cb0
1139556759@qq.com <fang15155259552>
2022-09-27 15:45:59 +0800

    init project

---
+++ src/test/java/com/dgcc/recycle_server/RecycleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgcc.recycle_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
